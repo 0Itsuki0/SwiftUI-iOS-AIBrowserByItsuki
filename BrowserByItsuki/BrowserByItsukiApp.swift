098f40f174612bd7bc1fed5675c8ba0563c1720f
itsuki <itsuki.enjyoy@gmail.com>
2025-10-25 08:52:25 +0900

    Initial Commit

---
+++ BrowserByItsuki/BrowserByItsukiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowserByItsukiApp.swift
+//  BrowserByItsuki
+//
+//  Created by Itsuki on 2025/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BrowserByItsukiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
